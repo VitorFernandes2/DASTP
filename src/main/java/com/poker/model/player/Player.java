4f778f6c06f5a4d29e816f2059bbfd5be3680c19
VitorFernandes2 <vhferprog@gmail.com>
2022-01-25 21:57:44 +0000

    DAS: refactor to the player

--- src/main/java/com/poker/model/player/Player.java
+++ src/main/java/com/poker/model/player/Player.java
@@ -1,34 +1,23 @@
 package com.poker.model.player;
 
 import com.poker.model.wallet.Wallet;
-import com.poker.logic.game.Game;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Player {
     private final String name;
-    private List<Game> currentGames;
-    private Wallet wallet;
+    private final Wallet wallet;
 
     public Player(String name) {
         this.name = name;
-        this.currentGames = new ArrayList<>();
         this.wallet = new Wallet(0, 0, 0);
     }
 
-    public Player(String name, List<Game> currentGames, Wallet wallet) {
+    public Player(String name, Wallet wallet) {
         this.name = name;
-        this.currentGames = currentGames;
         this.wallet = wallet;
     }
 
     public String getName() {
         return name;
-    }
-
-    public List<Game> getCurrentGames() {
-        return currentGames;
     }
 
     public Wallet getWallet() {
