b3de6114a275b740d8c49fb1bc735987b2b8ef4c
VitorFernandes2 <vhferprog@gmail.com>
2022-01-26 13:24:32 +0000

    DAS: refactor of the states

--- src/main/java/com/poker/logic/game/state/BuyInState.java
+++ src/main/java/com/poker/logic/game/state/BuyInState.java
@@ -3,7 +3,6 @@ package com.poker.logic.game.state;
 public class BuyInState extends StateAdapter {
     @Override
     public IGameState buyCoins() {
-        //TBD
         return super.buyCoins();
     }
 }
--- src/main/java/com/poker/logic/game/state/ShowCardsState.java
+++
@@ -1,8 +0,0 @@
-package com.poker.logic.game.state;
-
-public class ShowCardsState extends StateAdapter {
-    @Override
-    public IGameState bet() {
-        return super.bet();
-    }
-}
