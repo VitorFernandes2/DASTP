1c52ac1a0ea2438ada67cfd8eb7197647c839ae3
Luís António Moreira Ferreira da Silva <44380629+Luisilva99@users.noreply.github.com>
2022-02-18 00:59:09 +0000

    DAS: EndGame Call Scoring

--- src/main/java/com/poker/ui/TextUI.java
+++ src/main/java/com/poker/ui/TextUI.java
@@ -33,7 +33,7 @@ public class TextUI {
         while (running) {
             System.out.print("♠ POKER  $ ");
             String commandLine;
-            List<String> commandsList = LIST_GAMES; // DEBUG: change this Command List to change the mocked commands
+            List<String> commandsList = CREATE_GAME_WINNING; // DEBUG: change this Command List to change the mocked commands
             if (commandsList.isEmpty()) {
                 commandLine = StringUtils.readString();
             } else {
